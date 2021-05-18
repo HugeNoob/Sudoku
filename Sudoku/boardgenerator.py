be48166fe169b253e6f5bbed7aae6f63f8b80da3
HugeNoob <thelastoreointhejar@gmail.com>
2021-05-18 19:24:48 +0800

    implemented new generate full with shuffling

--- Sudoku/boardgenerator.py
+++ Sudoku/boardgenerator.py
@@ -1,17 +1,9 @@
 from sudokusolver import solve, validity, find
-import random
-import copy
+from sorted_solver import sort, sorted_solve
+import random, copy, time
 
-# Empty board template
-board = [[0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0]]
+# Complete board template
+board = [[7, 4, 6, 9, 5, 8, 1, 2, 3], [8, 9, 2, 3, 1, 6, 7, 4, 5], [5, 3, 1, 7, 2, 4, 9, 8, 6], [9, 2, 5, 6, 7, 3, 8, 1, 4], [6, 8, 7, 1, 4, 9, 3, 5, 2], [4, 1, 3, 2, 8, 5, 6, 9, 7], [3, 6, 8, 4, 9, 2, 5, 7, 1], [1, 5, 4, 8, 3, 7, 2, 6, 9], [2, 7, 9, 5, 6, 1, 4, 3, 8]]
 
 # Maps each coord on the board
 def mapping(rows, cols):
@@ -25,26 +17,35 @@ def mapping(rows, cols):
     return coords
 
 # Generates a full valid sudoku board
+def swap(board, i, j):
+    if i != j:
+        board[i], board[j] = board[j], board[i]
+
+# Shuffles rows within 3 rows
+def row_shuffle(board):
+    # Top
+    i, j = random.randint(0,2), random.randint(0,2)
+    swap(board, i, j)
+
+    # Middle
+    i, j = random.randint(3,5), random.randint(3,5)
+    swap(board, i, j)
+
+    # Bottom
+    i, j = random.randint(6,8), random.randint(6,8)
+    swap(board, i, j)
+    
+    return board
+
+# Shuffles board template to generate new board
 def generate_full(board):
-    coords = mapping(9,9)
+    row_shuffle(board)
 
-    for i in range(81):
-        # Finds random coord
-        coords_index = random.randint(0, len(coords) - 1)
-        row, col = curr_coord = coords[coords_index]
-        
-        # Fills coord with valid num
-        temp_board = copy.deepcopy(board)
-        for val in range(1,10):
-
-            if validity(temp_board, row, col, val):
-                temp_board[row][col] = val
-                
-                if solve(temp_board):
-                    board[row][col] = val
-                    break
-        coords.remove(curr_coord)
-            
+    # 50/50 chance to rotate board
+    if random.randint(0,1):
+        board = [[i[j] for i in board] for j in range(9)]
+    
+    row_shuffle(board)
     return board
 
 # Checks if current board has more than 1 solution
@@ -62,7 +63,7 @@ def solve_for_more(board, row, col):
         temp_board = copy.deepcopy(board)
         temp_board[row][col] = val
 
-        if solve(temp_board):
+        if sorted_solve(temp_board, sort(temp_board)):
             solutions += 1
         
         if solutions > 1:
@@ -76,7 +77,7 @@ def generate_sudoku(board):
     coords = mapping(9,9)
     
     # 65 is honestly an arbitrary number to increase efficiency
-    for i in range(60):
+    for i in range(65):
 
         # Finds random coord
         coords_index = random.randint(0, len(coords) - 1)
@@ -96,9 +97,3 @@ def generate_sudoku(board):
             coords.remove(curr_coord)
 
     return board
-
-
-
-#test_board = [[7, 4, 6, 9, 5, 8, 1, 2, 3], [8, 9, 2, 3, 1, 6, 7, 4, 5], [5, 3, 1, 7, 2, 4, 9, 8, 6], [9, 2, 5, 6, 7, 3, 8, 1, 4], [6, 8, 7, 1, 4, 9, 3, 5, 2], [4, 1, 3, 2, 8, 5, 6, 9, 7], [3, 6, 8, 4, 9, 2, 5, 7, 1], [1, 5, 4, 8, 3, 7, 2, 6, 9], [2, 7, 9, 5, 6, 1, 4, 3, 8]]
-#print(generate_sudoku(test_board))
-#print(generate_full(board))
