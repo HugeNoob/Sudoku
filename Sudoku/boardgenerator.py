05d80770bfb5d69b3cde757be32148401f46bba5
HugeNoob <thelastoreointhejar@gmail.com>
2021-05-23 16:51:24 +0800

    added docstring

--- Sudoku/boardgenerator.py
+++ Sudoku/boardgenerator.py
@@ -5,24 +5,33 @@ import random, copy, time
 # Complete board template
 board_template = [[7, 4, 6, 9, 5, 8, 1, 2, 3], [8, 9, 2, 3, 1, 6, 7, 4, 5], [5, 3, 1, 7, 2, 4, 9, 8, 6], [9, 2, 5, 6, 7, 3, 8, 1, 4], [6, 8, 7, 1, 4, 9, 3, 5, 2], [4, 1, 3, 2, 8, 5, 6, 9, 7], [3, 6, 8, 4, 9, 2, 5, 7, 1], [1, 5, 4, 8, 3, 7, 2, 6, 9], [2, 7, 9, 5, 6, 1, 4, 3, 8]]
 
-# Maps each coord on the board
 def mapping(rows, cols):
+    ''' 
+    Takes in integers rows and cols, and returns a list of coordinates in a row x col grid.
+    '''
     coords = []
     for row in range(rows):
         for col in range(cols):
             coord = (row, col)
             coords.append(coord)
-
+    range()
     # Returns 81 coords
     return coords
 
-# Swaps two lists
 def swap(board, i, j):
+    '''
+    Takes two lists in the board and swaps their positions.
+    '''
     if i != j:
         board[i], board[j] = board[j], board[i]
 
-# Shuffles rows within 3 rows
 def row_shuffle(board):
+    '''
+    Returns shuffled board.
+
+    Rows are shuffled within their groups of three rows — 0 to 2, 3 to 5, 6 to 8.
+    The shuffling is done by choosing two random rows and swapping them.
+    '''
     # Top
     i, j = random.randint(0,2), random.randint(0,2)
     swap(board, i, j)
@@ -37,8 +46,14 @@ def row_shuffle(board):
 
     return board
 
-# Shuffles board template to generate new board
 def generate_full(board):
+    '''
+    Shuffles and returns a full, valid board template to generate a new, full, valid board.
+
+    Initially, rows are shuffled with row_shuffle.
+    Then, there is a 50% chance to rotate the board 90 degrees.
+    Lastly, rows are shuffled again with row_shuffle. Note that if rotation is done, cols will then act as rows.
+    '''
     row_shuffle(board)
 
     # 50/50 chance to rotate board
@@ -48,8 +63,13 @@ def generate_full(board):
     row_shuffle(board)
     return board
 
-# Checks if current board has more than 1 solution
 def solve_for_more(board, row, col):
+    '''
+    Checks if current board has more than 1 solution.
+
+    Returns 1 if only 1 solution.
+    Returns 2 if more than 1 solution.
+    '''
     valid_vals = []
     solutions = 0
 
@@ -71,8 +91,12 @@ def solve_for_more(board, row, col):
         
     return solutions
 
-# Generates sudoku board with 0s from a valid full board
 def generate_sudoku(board, difficulty):
+    '''
+    Generates and returns a partially filled sudoku board from a full, valid board.
+
+    Takes in an integer difficulty, and analyses a corresponding number of random coordinates to determine if they can be removed.
+    '''
     # Generates map coords
     coords = mapping(9,9)
 
